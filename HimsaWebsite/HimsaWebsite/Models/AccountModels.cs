24b5885c5e4c8b31e9a86a085cdc109b89765336
Osama Taleb Alghanmi <osamah.net.m@gmail.com>
2013-02-24 11:29:32 -0500

    Project Plan+DB change
    Added project mind map. Changes to some database tables (Tasks, User) .
    Minor changes to the cod

--- HimsaWebsite/HimsaWebsite/Models/AccountModels.cs
+++ HimsaWebsite/HimsaWebsite/Models/AccountModels.cs
@@ -18,15 +18,6 @@ namespace HimsaWebsite.Models
         //new comment
         public DbSet<UserProfile> UserProfiles { get; set; }
         public DbSet<app_message> app_message { get; set; }
-        public DbSet<badge> badges { get; set; }
-        public DbSet<lvl_cap> lvl_cap { get; set; }
-        public DbSet<setting> settings { get; set; }
-        public DbSet<task> tasks { get; set; }
-        public DbSet<team> teams { get; set; }
-        public DbSet<user_team> user_team { get; set; }
-        //public DbSet<webpages_Membership> webpages_Membership { get; set; }
-        public DbSet<webpages_OAuthMembership> webpages_OAuthMembership { get; set; }
-        public DbSet<webpages_Roles> webpages_Roles { get; set; }
     }
 
     [Table("UserProfile")]
@@ -64,94 +55,12 @@ namespace HimsaWebsite.Models
         public int message_id { get; set; }
 
         public string name { get; set; }
-        public string message_type { get; set; }
-    }
-
-    public  class badge
-    {
-        [Key]
-        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
-        public int badge_id { get; set; }
-        public int usr_id { get; set; }
-        public byte[] img { get; set; }
-
-        public virtual UserProfile UserProfile { get; set; }
+       
     }
 
 
-    public  class lvl_cap
-    {
-        [Key]
-        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
-        public int lvl_no { get; set; }
-        public Nullable<int> cap { get; set; }
-    }
-    public  class setting
-    {
-        [Key]
-        public int idsettings { get; set; }
-    }
 
 
-    public  class task
-    {
-        [Key]
-        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
-        public int task_id { get; set; }
-
-        public string name { get; set; }
-        public Nullable<int> xp { get; set; }
-        public Nullable<int> usr_id { get; set; }
-        public Nullable<int> complete { get; set; }
-        public string message_type { get; set; }
-        public Nullable<System.DateTime> end_date { get; set; }
-        public string descrp { get; set; }
-        public byte[] descrp_story { get; set; }
-        public Nullable<int> team_id { get; set; }
-        public Nullable<int> difficulty { get; set; }
-
-        public virtual team team { get; set; }
-        public virtual UserProfile UserProfile { get; set; }
-    }
-
-    public  class team
-    {
-        public team()
-        {
-            this.tasks = new HashSet<task>();
-            this.user_team = new HashSet<user_team>();
-        }
-
-
-        [Key]
-        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
-        public int team_id { get; set; }
-        public string name { get; set; }
-        public byte[] imag { get; set; }
-        public Nullable<int> progress { get; set; }
-
-        public virtual ICollection<task> tasks { get; set; }
-        public virtual ICollection<user_team> user_team { get; set; }
-    }
-
-
-    public  class user_team
-    {
-        [Key]
-        [Column(Order = 0)]
-        public int user_team_id { get; set; }
-        [Key]
-        [Column(Order = 1)]
-        public int usr_id { get; set; }
-        [Key]
-        [Column(Order = 2)]
-        public int team_id { get; set; }
-
-        public System.DateTime join_date { get; set; }
-
-        public virtual team team { get; set; }
-        public virtual UserProfile UserProfile { get; set; }
-    }
 
 
      [Table("webpages_Membership")]
