2fa4ed5735cebb8219e5dea528a830e2db399ab3
javasop <osamah.net.m@gmail.com>
2013-02-25 09:13:08 -0500

    changes to the db model

---
+++ HimsaWebApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HimsaWebApp::Application.config.secret_token = '214de3c7cfb6bd06cdc103cdd0a07f3ada7713d89fcddbf93ef43361ea7faf06f1d6b82f75b0e275f80fa85661853379391a1d6f007f6ea5c3c852a8ae5d441f'
